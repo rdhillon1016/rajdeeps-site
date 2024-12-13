c60fcde30cbfe3c6ba5ef94d14a1cde8ad4d2122
Rajdeep Dhillon <rdhillon1016@gmail.com>
2024-12-12 19:30:04 -0800

    added more html and incorporated tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://rdhillon1016.github.io'
+  site: 'https://rdhillon1016.github.io',
+  integrations: [tailwind()]
 });
---
+++ src/misc-content/intro.md
@@ -0,0 +1,3 @@
+# About me
+
+Hello visitors. My name is Rajdeep Dhillon and I am a software developer who has constructed this personal blog with the intention of combining my passions for programming and writing. 
