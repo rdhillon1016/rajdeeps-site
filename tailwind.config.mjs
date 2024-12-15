5bea3a6f6aa2da51471d6b73203a9818cf3188ff
Rajdeep Dhillon <rdhillon1016@gmail.com>
2024-12-14 21:17:20 -0800

    added some better styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			primary: colors.zinc,
+			dark_accent: colors.teal[200],
+			light_accent: colors.blue,
+			black: colors.black
+		}
 	},
 	plugins: [],
 	darkMode: 'selector'
